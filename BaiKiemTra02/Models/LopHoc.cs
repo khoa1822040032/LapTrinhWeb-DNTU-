6dfc1617edff983c99ad3f8cc29bb36134a3a4cd
khoa0803 <1822040032@dntu.edu.vn>
2024-09-28 15:11:39 +0700

    baitap backup

---
+++ BaiKiemTra02/Models/LopHoc.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaiKiemTra02.Models
+{
+	public class LopHoc
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required(ErrorMessage = "Không được để trông tên lớp học")]
+		[Display(Name ="Tên lớp học")]
+		public string TenLopHoc { get; set; }
+		[Required(ErrorMessage = "Không được để trông năm nhập học")]
+		[Display(Name = "Năm Nhập Học")]
+		public string NamNhapHoc { get; set; }
+		[Required(ErrorMessage = "Không được để trông năm ra trường")]
+		[Display(Name = "Năm ra trường")]
+		public string NamRaTruong { get; set;}
+		[Required(ErrorMessage = "Không được để trông số lượng sinh viên")]
+		[Display(Name = "Số lượng sinh viên")]
+		public int SoLuongSinhVien {  get; set; }
+	}
+}
