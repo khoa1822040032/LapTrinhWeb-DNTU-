966d49251cb1015e5265b41aecbc6504e2a56be4
khoa0803 <1822040032@dntu.edu.vn>
2024-09-28 15:26:48 +0700

    bkt

---
+++ BaiKiemTra02/BaiKiemTra02/Controllers/LopHocController.cs
@@ -0,0 +1,105 @@
+﻿using BaiKiemTra02.Data;
+using BaiKiemTra02.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiKiemTra02.Controllers
+{
+    public class LopHocController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public LopHocController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var lophoc = _db.lophoc.ToList();
+            ViewBag.lophoc = lophoc;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(LopHoc lophoc)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.lophoc.Add(lophoc);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var lophoc = _db.lophoc.Find(id);
+            return View(lophoc);
+        }
+        [HttpPost]
+
+        public IActionResult Edit(LopHoc lophoc)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.lophoc.Update(lophoc);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var lophoc = _db.lophoc.Find(id);
+            return View(lophoc);
+        }
+        [HttpPost]
+        public IActionResult DeleteConfirm(int id)
+        {
+            var lophoc = _db.lophoc.Find(id);
+            if (lophoc == null)
+            {
+                return NotFound();
+            }
+            _db.lophoc.Remove(lophoc);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+
+            var lophoc = _db.lophoc.Find(id);
+
+            if (lophoc == null)
+            {
+                return NotFound();
+            }
+
+            return View(lophoc);
+        }
+        [HttpPost]
+        public IActionResult DetailsPost(int id)
+        {
+            return Forbid();
+        }
+
+
+    }
+}
---
+++ BaiKiemTra02/BaiKiemTra02/Models/LopHoc.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BaiKiemTra02.Models
+{
+    public class LopHoc
+    {
+        
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Tên lớp học là bắt buộc.")]
+        public string TenLopHoc { get; set; }
+
+        [Range(1900, 2100, ErrorMessage = "Năm nhập học phải nằm trong khoảng từ 1900 đến 2100.")]
+        public int NamNhapHoc { get; set; }
+
+        [Range(1900, 2100, ErrorMessage = "Năm ra trường phải nằm trong khoảng từ 1900 đến 2100.")]
+        public int NamRaTruong { get; set; }
+
+        [Range(1, 1000, ErrorMessage = "Số lượng sinh viên phải nằm trong khoảng từ 1 đến 1000.")]
+        public int SoLuongSinhVien { get; set; }
+    }
+}
+
