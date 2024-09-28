d3a90fb9ed5f5c40336e9a09f255c852459b4299
khoa0803 <1822040032@dntu.edu.vn>
2024-09-28 14:21:50 +0700

    baitap backup

---
+++ BaiTapKT01/Models/TaiKhoanViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BaiTap06.Models
+{
+    public class TaiKhoanViewModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string old { get; set; }
+        public string Name { get; set; }
+    }
+}
