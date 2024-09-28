2f9bb7e887d0a4374ae391a7bd146747c1f31e1b
khoa0803 <1822040032@dntu.edu.vn>
2024-09-28 15:41:05 +0700

    bkt

--- BaiKiemTra02/BaiKiemTra02/obj/Debug/net8.0/BaiKiemTra02.AssemblyInfo.cs
+++ BaiKiemTra02/BaiKiemTra02/obj/Debug/net8.0/BaiKiemTra02.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaiKiemTra02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+304b4f74b40ecf4045dde678011423fd4dc02f29")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaiKiemTra02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaiKiemTra02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
